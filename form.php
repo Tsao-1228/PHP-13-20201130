a30712884806fc85d90a04f47453a08f48d98295
Zac <a19860101@gmail.com>
2020-12-07 20:03:38 +0800

    20201207

---
+++ form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <div>
+            <label for="name">姓名</label>
+            <input type="text" id="name" name="name">
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email">
+        </div>
+        <input type="submit" value="送出">
+    </form>
+</body>
+</html>
