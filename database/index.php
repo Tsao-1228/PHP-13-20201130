8231691caa9d3f6dbae0826fa896997bd61ad0f2
Zac <a19860101@gmail.com>
2020-12-14 19:44:45 +0800

    20201214

--- database/index.php
+++ database/index.php
@@ -1,3 +1,20 @@
+<?php
+    require_once("conn.php");
+
+    $sql = "SELECT * FROM students";
+    $result = mysqli_query($conn,$sql);
+
+    // $row = mysqli_fetch_assoc($result); //關聯陣列
+    // $row = mysqli_fetch_array($result); //關聯陣列與一班陣列
+    // $row = mysqli_fetch_row($result); //一般陣列
+
+    while($row = mysqli_fetch_assoc($result)){
+        echo $row["name"];
+        echo $row["skills"];
+        echo "<br>";
+    }    
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
