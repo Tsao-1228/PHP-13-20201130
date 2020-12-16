bb4a08f65ed27eae52578b9de56b2723bf313b23
Zac <a19860101@gmail.com>
2020-12-16 21:07:30 +0800

    20201216

--- pdo/store.php
+++ pdo/store.php
@@ -1,11 +1,30 @@
 <?php
+	// try {
+    //     require_once("pdo.php");
+    //     extract($_POST);
+    //     $skills = implode(",",$skills);
+    //     $sql = "INSERT INTO students(name,email,phone,gender,edu,skills,content)VALUES(?,?,?,?,?,?,?)";
+	// 	$stmt = $pdo->prepare($sql);
+	// 	$stmt->execute([$name,$email,$phone,$gender,$edu,$skills,$content]);
+        
+	// }catch(PDOException $e){
+    //     echo $e->getMessage();
+	// }
 	try {
         require_once("pdo.php");
         extract($_POST);
         $skills = implode(",",$skills);
-        $sql = "INSERT INTO students(name,email,phone,gender,edu,skills,content)VALUES(?,?,?,?,?,?,?)";
+        $sql = "INSERT INTO students(name,email,phone,gender,edu,skills,content)
+				VALUES(:name,:email,:phone,:gender,:edu,:skills,:content)";
 		$stmt = $pdo->prepare($sql);
-		$stmt->execute([$name,$email,$phone,$gender,$edu,$skills,$content]);
+		$stmt->bindParam(":name",$name);
+		$stmt->bindParam(":phone",$phone);
+		$stmt->bindParam(":email",$email);
+		$stmt->bindParam(":gender",$gender);
+		$stmt->bindParam(":edu",$edu);
+		$stmt->bindParam(":skills",$skills);
+		$stmt->bindParam(":content",$content);
+		$stmt->execute();
         
 	}catch(PDOException $e){
         echo $e->getMessage();
