9fab73480094dba924e4c74f124ffdb419289afa
Zac <a19860101@gmail.com>
2020-12-16 21:14:28 +0800

    20201216

--- pdo/delete.php
+++ pdo/delete.php
@@ -1,7 +1,22 @@
 <?php
-    require_once("conn.php");
-    // $id = $_POST["id"];
-    extract($_POST);
-    $sql = "DELETE FROM students WHERE id = {$id}";
-    mysqli_query($conn,$sql);
+    try {
+        require_once("pdo.php");
+        extract($_POST);
+        $sql = "DELETE FROM students WHERE id = ?";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$id]);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+    // try {
+    //     require_once("pdo.php");
+    //     extract($_POST);
+    //     $sql = "DELETE FROM students WHERE id = :id";
+    //     $stmt = $pdo->prepare($sql);
+    //     $stmt->bindParam(":id",$id);
+    //     $stmt->execute();
+    // }catch(PDOException $e){
+    //     echo $e->getMessage();
+    // }
+
     header("location:index.php");
--- pdo/store.php
+++ pdo/store.php
@@ -1,32 +1,32 @@
 <?php
-	// try {
-    //     require_once("pdo.php");
-    //     extract($_POST);
-    //     $skills = implode(",",$skills);
-    //     $sql = "INSERT INTO students(name,email,phone,gender,edu,skills,content)VALUES(?,?,?,?,?,?,?)";
-	// 	$stmt = $pdo->prepare($sql);
-	// 	$stmt->execute([$name,$email,$phone,$gender,$edu,$skills,$content]);
-        
-	// }catch(PDOException $e){
-    //     echo $e->getMessage();
-	// }
 	try {
         require_once("pdo.php");
         extract($_POST);
         $skills = implode(",",$skills);
-        $sql = "INSERT INTO students(name,email,phone,gender,edu,skills,content)
-				VALUES(:name,:email,:phone,:gender,:edu,:skills,:content)";
+        $sql = "INSERT INTO students(name,email,phone,gender,edu,skills,content)VALUES(?,?,?,?,?,?,?)";
 		$stmt = $pdo->prepare($sql);
-		$stmt->bindParam(":name",$name);
-		$stmt->bindParam(":phone",$phone);
-		$stmt->bindParam(":email",$email);
-		$stmt->bindParam(":gender",$gender);
-		$stmt->bindParam(":edu",$edu);
-		$stmt->bindParam(":skills",$skills);
-		$stmt->bindParam(":content",$content);
-		$stmt->execute();
+		$stmt->execute([$name,$email,$phone,$gender,$edu,$skills,$content]);
         
 	}catch(PDOException $e){
         echo $e->getMessage();
 	}
+	// try {
+    //     require_once("pdo.php");
+    //     extract($_POST);
+    //     $skills = implode(",",$skills);
+    //     $sql = "INSERT INTO students(name,email,phone,gender,edu,skills,content)
+	// 			VALUES(:name,:email,:phone,:gender,:edu,:skills,:content)";
+	// 	$stmt = $pdo->prepare($sql);
+	// 	$stmt->bindParam(":name",$name);
+	// 	$stmt->bindParam(":phone",$phone);
+	// 	$stmt->bindParam(":email",$email);
+	// 	$stmt->bindParam(":gender",$gender);
+	// 	$stmt->bindParam(":edu",$edu);
+	// 	$stmt->bindParam(":skills",$skills);
+	// 	$stmt->bindParam(":content",$content);
+	// 	$stmt->execute();
+        
+	// }catch(PDOException $e){
+    //     echo $e->getMessage();
+	// }
 	header("location:index.php");
