de887e52572632d356d2b7c9bbdd157083365d64
Zac <a19860101@gmail.com>
2020-12-16 20:02:57 +0800

    20201216

--- pdo/pdo.php
+++ pdo/pdo.php
@@ -6,5 +6,13 @@
     $db_charset = "utf8mb4";
 
     $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
-    $pdo = new PDO($dsn,$db_user,$db_pw);
-    //啟用pdo類別
+    // $pdo = new PDO($dsn,$db_user,$db_pw);
+    
+
+    // 例外處理,錯誤處理,異常處理
+    try {
+        $pdo = new PDO($dsn,$db_user,$db_pw);
+        //啟用pdo類別
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
