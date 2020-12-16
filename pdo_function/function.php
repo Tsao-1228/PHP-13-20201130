14531cd536cd84e022b54be4ab913b0cc3a9f1c5
Zac <a19860101@gmail.com>
2020-12-16 21:55:39 +0800

    20201216

--- pdo_function/function.php
+++ pdo_function/function.php
@@ -15,3 +15,16 @@
             echo $e->getMessage();
         }
     }
+    function store($post){
+        try {
+            require_once("pdo.php");
+            extract($post);
+            $skills = implode(",",$skills);
+            $sql = "INSERT INTO students(name,email,phone,gender,edu,skills,content)VALUES(?,?,?,?,?,?,?)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$name,$email,$phone,$gender,$edu,$skills,$content]);
+            
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
