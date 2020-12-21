0e6068e8dc041f03af3ad2fc47d9d47055b0b9a2
Zac <a19860101@gmail.com>
2020-12-21 20:15:16 +0800

    20201221

--- file_stystem/upload.php
+++ file_stystem/upload.php
@@ -6,8 +6,12 @@
     // $_FILES["img"]["size"]
     // $_FILES["img"]["tmp_name"]
     // $_FILES["img"]["type"]
+
+    $img_name = md5(time());
+    $target = "images/{$img_name}";
+    
     if($error == 0){
-        if(move_uploaded_file($tmp_name,"images/0.jpg")){
+        if(move_uploaded_file($tmp_name,$target)){
             echo "上傳成功";
         }else{
             echo "上傳失敗";
