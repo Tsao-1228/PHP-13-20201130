dd45bf9a52cb9e5a6c6762f83f3ad9b4c27101e3
Zac <a19860101@gmail.com>
2020-12-28 19:12:52 +0800

    20201228

--- member/function.php
+++ member/function.php
@@ -12,11 +12,9 @@
             $stmt->execute([$user]);
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
             
-            // if(!$row){
-            //     return 1;
-            // }
-
-            return $row ? "":1;
+            if(!$row){
+                return 1;
+            }
             
             if($row["pw"] === $pw){
                 $_SESSION["AUTH"] = $row;
